35fb31fbc286ae761f717cf9476a71315cdf1f2f
AJ <andrew_jones@outlook.com>
2015-10-08 20:38:45 +0200

    Added more user data to user object

--- TwitchWrapper.Tests/UserClientTests.cs
+++ TwitchWrapper.Tests/UserClientTests.cs
@@ -1,18 +1,67 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
 
 namespace TwitchWrapper.Tests
 {
     [TestFixture]
     class UserClientTests
     {
-        [Test]
-        public void GetUserShouldReturnAUserWithAName()
+        private User _user;
+
+        [SetUp]
+        public void GivenACallToTheUserEndpoint()
         {
             var client = new UserClient(new TwitchClient());
+            _user = client.GetUser("test_user1");
+        }
 
-            var user = client.GetUser("test_user1");
+        [Test]
+        public void ThenTheCorrectIdShouldBeReturned()
+        {
+            Assert.That(_user.Id, Is.EqualTo(22747064));
+        }
 
-            Assert.That(user.Display_name, Is.EqualTo("Test_user1"));
+        [Test]
+        public void ThenTheCorrectNameShouldBeReturned()
+        {
+            Assert.That(_user.Name, Is.EqualTo("test_user1"));
+        }
+
+        [Test]
+        public void ThenTheCorrectDisplayNameShouldBeReturned()
+        {
+            Assert.That(_user.DisplayName, Is.EqualTo("Test_user1"));
+        }
+        
+        [Test]
+        public void ThenTheCorrectUserTypeShouldBeReturned()
+        {
+            Assert.That(_user.Type, Is.EqualTo("user"));
+        }
+
+        [Test]
+        public void ThenIfNoBioIsSetThenBioShouldBeNull()
+        {
+            Assert.That(_user.Bio, Is.Null);
+        }
+
+        [Test]
+        public void ThenTheDateCreatedAtDateIsReturnedAsTheCorrectDateTime()
+        {
+            Assert.That(_user.CreatedAt, Is.EqualTo(new DateTime(2011, 6, 2, 20, 4, 3)));
+        }
+
+        [Test]
+        public void ThenTheDateUpdatedAtDateIsReturnedAsTheCorrectDateTime()
+        {
+            Assert.That(_user.UpdatedAt, Is.EqualTo(new DateTime(2015, 9, 16, 1, 15, 11)));
+        }
+
+        [Test]
+        public void ThenTheLogoUrlIsReturned()
+        {
+            Assert.That(_user.Logo, Is.EqualTo("http://static-cdn.jtvnw.net/jtv_user_pictures/test_user1-profile_image-ac0a2f0d39dda770-300x300.jpeg"));
         }
     }
 }
--- TwitchWrapper/User.cs
+++ TwitchWrapper/User.cs
@@ -1,7 +1,17 @@
+using System;
+using System.Collections.Generic;
+
 namespace TwitchWrapper
 {
     public class User
     {
-        public string Display_name { get; set; }
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Bio { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string Logo { get; set; }
     }
 }
